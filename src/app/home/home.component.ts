d33fdd8e64ca57befae21ca29cacdccd77aff96e
GSF417 <gsferreira16unifesp@gmail.com>
2022-05-16 14:40:40 -0300

    1 binaries uploaded

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,17 +3,18 @@ import { UserService } from '../_services/user.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  imageUnifespLogo = 'https://angular-ivy-nzyxem.stackblitz.io/src/assets/images/unifespLogo.png';
   content?: string;
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {}
   ngOnInit(): void {
     this.userService.getPublicContent().subscribe(
-      data => {
+      (data) => {
         this.content = data;
       },
-      err => {
+      (err) => {
         this.content = JSON.parse(err.error).message;
       }
     );
